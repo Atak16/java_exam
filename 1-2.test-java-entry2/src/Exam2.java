f741aba42f75c669d256c3dbf82a101f67f4d94f
Akihide Takahashi <akihide.tak16@gmail.com>
2026-01-16 11:24:30 +0900

    プログラミング入門2

---
+++ 1-2.test-java-entry2/src/Exam2.java
@@ -0,0 +1,10 @@
+public class Exam2 {
+    public static void main(String[] args) {
+        while (int i = 1) {
+            i <= 10;
+            i++;
+            System.out.println();
+
+        }
+    }
+}
