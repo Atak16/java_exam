be10b0d6e6fdd7895513e05a802e35233363c5bb
Akihide Takahashi <akihide.tak16@gmail.com>
2026-01-22 10:00:01 +0900

    オブジェクト応用

---
+++ 3.test-object-oriented2/src/exam1/Exam1.java
@@ -0,0 +1,12 @@
+package exam1;
+
+public class Exam1 {
+    public static void main(String[] args) {
+        SuperCar car = new SuperCar();
+
+        car.name;
+        car.voidrun();
+
+    }
+    
+}
